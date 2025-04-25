8573500216626fd64a1f8d18362031114f393c3c
Amdadul Haq <amdadulujjal@gmail.com>
2025-04-25 13:24:29 -0400

    Finished with index.js for server

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const authRoutes = require('./routes/auth');
+const eventRoutes = require('./routes/events');
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/events', eventRoutes);
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('MongoDB connected');
+  app.listen(5000, () => console.log('Server running on port 5000'));
+}).catch(err => console.error(err));
